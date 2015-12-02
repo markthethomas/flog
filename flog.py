83316252bdae61f06e91206e374ae00cd6fd54a0
Mark Thomas <markthethomas@gmail.com>
2015-12-01 22:04:14 -0800

    Trying out rendering using jinja2

--- flog.py
+++ flog.py
@@ -1,20 +1,23 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 app.debug = True
 
+
 @app.route('/', methods=['GET'])
 def index():
-    return 'Hello World!'
+    return 'Home!'
+
 
 @app.route('/login', methods=['GET', 'POST'])
-def login(username):
-    return username
+def login():
+    return 'login!'
+
 
 @app.route('/<year>/<month>/<day>/<name>', methods=['GET'])
 def fname(year, month, day, name):
-    return year
+    return render_template('layout.html', year=year, month=month, day=day, name=name)
 
 if __name__ == '__main__':
     app.run()
